8a229119c07d2fd19aeb6b0f6b676bc4976a10cd
Andrea Ascari <andrea.ascari@buildo.io>
2016-06-11 23:03:56 +0200

    Added logic and content to Order component.

--- web/src/app/components/customer/Order/Order.js
+++ web/src/app/components/customer/Order/Order.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import t from 'tcomb';
 import { props } from 'tcomb-react';
 import { skinnable } from 'revenge';
 import { FlexView } from 'Basic';
@@ -7,14 +8,66 @@ import { Order as OrderType } from 'model';
 
 @skinnable()
 @props({
-  order: OrderType
+  order: OrderType,
+  onAddPersonClick: t.Function,
+  onPersonClick: t.Function,
+  onDeletePersonClick: t.Function
 })
 export default class Order extends React.Component {
 
-  template(/*{ order }*/) {
+  onAddPersonClick = () => {
+    const personName = window.prompt('Inserisci il nome della persona');
+    personName.trim() && this.props.onAddPersonClick(personName.trim());
+  }
+
+  onDeletePersonClick = (personName) => (e) => {
+    e.stopPropagation();
+    if (window.confirm(`Eliminare l\'ordine di ${personName}?`)) {
+      this.props.onDeletePersonClick(personName);
+    }
+  }
+
+  templatePersonItem = (item, key) => (
+    <FlexView key={key}>
+      {item}
+    </FlexView>
+  )
+
+  templatePerson = ({ onPersonClick, onDeletePersonClick }) => (person, key) => (
+    <FlexView key={key} column>
+      <FlexView onClick={onPersonClick(person.name)}>
+        <FlexView grow>
+          {person.name}
+        </FlexView>
+        <FlexView hAlignContent='right' onClick={onDeletePersonClick(person.name)}>
+          x
+        </FlexView>
+      </FlexView>
+      <FlexView className='items' column>
+        {person.items.map(this.templatePersonItem)}
+      </FlexView>
+    </FlexView>
+  )
+
+  getLocals({ order, onPersonClick }) {
+    return {
+      order,
+      onAddPersonClick: this.onAddPersonClick,
+      onPersonClick,
+      onDeletePersonClick: this.onDeletePersonClick
+    };
+  }
+
+  template({ order, onPersonClick, onDeletePersonClick, onAddPersonClick }) {
     return (
       <FlexView className='order' column>
-        Order goes here...
+        <FlexView className='people' column>
+          {order.people.map(this.templatePerson({ onPersonClick, onDeletePersonClick }))}
+        </FlexView>
+        <button onClick={onAddPersonClick}>
+          Add person
+        </button>
+
       </FlexView>
     );
   }
--- web/src/app/components/customer/Order/OrderContainer.js
+++ web/src/app/components/customer/Order/OrderContainer.js
@@ -2,5 +2,15 @@ import container from 'container';
 import Order from './Order';
 
 export default container(Order, {
-  queries: ['order']
+  connect: { },
+  commands: ['doAddPersonToOrder', 'doDeletePersonFromOrder'],
+  queries: ['order'],
+  mapProps: ({ transition, doAddPersonToOrder, doDeletePersonFromOrder, order }) => ({
+    order,
+    onAddPersonClick: (personName) => doAddPersonToOrder(personName),
+    onPersonClick: (personName) => () => {
+      transition({ view: 'personOrder', personName });
+    },
+    onDeletePersonClick: (personName) => doDeletePersonFromOrder(personName)
+  })
 });
