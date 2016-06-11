bd055abfbcce0aa1b1581c4251b1685380bf6914
Francesco <firefelix@gmail.com>
2016-06-11 19:35:04 +0200

    move common menu style to menu.scss

--- web/src/app/components/customer/StaticMenu/StaticMenu.js
+++ web/src/app/components/customer/StaticMenu/StaticMenu.js
@@ -4,7 +4,7 @@ import { skinnable } from 'revenge';
 import { FlexView } from 'Basic';
 import { Menu as MenuType } from 'model';
 
-import './staticMenu.scss';
+import '../Menu/menu.scss';
 
 @skinnable()
 @props({
@@ -13,16 +13,16 @@ import './staticMenu.scss';
 export default class StaticMenu extends React.Component {
 
   templateMenuGroupItem = (item, key) => (
-    <FlexView className='item' key={key} column>
+    <FlexView className='menu-item' key={key} column>
       <h4>{item.name}</h4>
       <div>{item.description}</div>
     </FlexView>
   )
 
   templateMenuGroup = (group, key) => (
-    <FlexView className='group' key={key} column>
+    <FlexView className='menu-group' key={key} column>
       <h3>{group.name}</h3>
-      <FlexView className='items' column>
+      <FlexView className='menu-items' column>
         {group.items.map(this.templateMenuGroupItem)}
       </FlexView>
     </FlexView>
@@ -30,10 +30,10 @@ export default class StaticMenu extends React.Component {
 
   template({ menu }) {
     return (
-      <FlexView className='menu' column>
+      <FlexView className='static-menu' column>
         <h2>{menu.description}</h2>
-        <FlexView className='groups' hAlignContent='center' column>
-        {menu.groups.map(this.templateMenuGroup)}
+        <FlexView className='menu-groups' hAlignContent='center' column>
+          {menu.groups.map(this.templateMenuGroup)}
         </FlexView>
       </FlexView>
     );
