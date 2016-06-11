e2c9e894299084ed6d2d0289dbff7a253bf4c79d
Marco Fumagalli <fuma900@gmail.com>
2016-06-11 23:48:43 +0200

    implement RestaurantCard

--- web/src/app/components/customer/RestaurantCard/RestaurantCard.js
+++ web/src/app/components/customer/RestaurantCard/RestaurantCard.js
@@ -1,39 +1,40 @@
 import React from 'react';
 import { props, t } from 'tcomb-react';
 import { pure, skinnable } from 'revenge';
-import FlexView from 'Basic';
+import { FlexView } from 'Basic';
+import { RestaurantProfile } from 'model';
 
 import './restaurantCard.scss';
+import './img/location-icon.png';
+import './img/time-icon.png';
 
 @pure
 @skinnable()
 @props({
-  //imageURL: t.String,
-  //name: t.String,
-  //timeSlot t.Array,
-  //address: t.String,
-  //onClick: t.Function,
+  profile: RestaurantProfile,
+  onClick: t.Function
 })
 export default class RestaurantCard extends React.Component {
 
   getLocals() {
-    const {name, imageURL, address, onClick, timeSlot = []} = this.props;
-    const [timeStart, timeEnd] = timeSlot;
-    return {name, imageURL, address, onClick, timeStart, timeEnd};
+    const { name, telephone, address, imageURL } = this.props.profile;
+    const onClick = this.props.onClick;
+    // const [timeStart, timeEnd] = timeSlot;
+    return { name, telephone, address, imageURL, onClick };
   }
 
-  template({name, imageURL, address, onClick, timeStart, timeEnd}) {
+  template({ name, imageURL, address, onClick, timeStart, timeEnd }) {
     return (
-      <FlexView column vAlignContent='center' className='restaurant-card' onClick={onClick}>
-        <div>
-          <img src={imageURL} alt={name} />
-          <p className='time-slot'>{timeStart} - {timeEnd}</p>
-          <div className='description'>
-            <p className='name'></p>
-            <p className='address'></p>
+      <div className='restaurant-card' onClick={onClick}>
+        <img src='http://placehold.it/285x152' alt={name} />
+        <div className='description'>
+          <div className='description-container'>
+            <p className='name'>{name}</p>
+            <i className='location-icon'></i> <p className='address'>{address}</p>
+            <i className='time-icon'></i>     <p className='time-slot'>12:30 - 14:55</p>
           </div>
         </div>
-      </FlexView>
+      </div>
     );
   }
 
