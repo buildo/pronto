a4bdd7b4deb2357a04652ede518a56ba210815f9
Giovanni Gonzaga <giovanni@buildo.io>
2016-06-11 17:04:58 +0200

    use the same structure for customers routes

--- web/src/app/routes/customer/index.js
+++ web/src/app/routes/customer/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router';
-import App from 'AppHandler';
-import Restaurants from 'RestaurantsHandler';
+import App from '../AppHandler';
+import Restaurants from './RestaurantsHandler';
 
 export default (
   <Route path='/' handler={App}>
