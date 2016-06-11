2db4842f748fc16a24d233162f8c4e3d23077b93
Andrea Ascari <andrea.ascari@buildo.io>
2016-06-12 00:01:56 +0200

    Removed nested RightSidebar component.

--- web/src/app/routes/customer/CreateOrderHandler.js
+++ web/src/app/routes/customer/CreateOrderHandler.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import CreateOrder from 'customer/CreateOrder';
-import RightSidebar from 'customer/RightSidebar';
 
 export default class CreateOrderHandler extends React.Component {
   render() {
-    return (
-      <RightSidebar>
-        <CreateOrder />
-      </RightSidebar>
-    );
+    return <CreateOrder />;
   }
 }
