57c4c0ba8760738bf98676bcb3d5f34a3c714ec4
Giovanni Gonzaga <giovanni@buildo.io>
2016-06-11 16:39:17 +0200

    cleanup

--- web/src/app/assets.js
+++ web/src/app/assets.js
@@ -1,4 +1,3 @@
 // FIXME (gio): copy here all static assets and/or webpack externals for now
 
 import 'theme';
-import 'buildo-react-components/src/flex/flexView.scss';
--- web/src/app/components/Basic/FlexView/index.js
+++ web/src/app/components/Basic/FlexView/index.js
@@ -1,1 +1,2 @@
+import 'buildo-react-components/src/flex/flexView.scss';
 export default from 'buildo-react-components/src/flex/FlexView';
