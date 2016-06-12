827aa53d47206ed1be5704c6688db956f3c7cfdc
Francesco <firefelix@gmail.com>
2016-06-12 19:51:36 +0200

    link RestaurantDetailsHeader to queries

--- web/src/app/components/customer/RestaurantDetailsHeader/RestaurantDetailsHeader.js
+++ web/src/app/components/customer/RestaurantDetailsHeader/RestaurantDetailsHeader.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { props, t } from 'tcomb-react';
+import { props } from 'tcomb-react';
 import { pure, skinnable } from 'revenge';
 import { Restaurant } from 'model';
 import FlexView from 'FlexView';
@@ -11,22 +11,15 @@ import './img/time-icon.png';
 @pure
 @skinnable()
 @props({
-  //restaurant: Restaurant
+  restaurant: Restaurant
 })
 export default class RestaurantDetailsHeader extends React.Component {
 
-  /*
-  getLocals({ restaurant: { name, address }}) {
-    // const [timeStart, timeEnd] = timeSlot;
+  getLocals({ restaurant: { name, address } }) {
     return { name, address };
   }
-  */
 
-  template({ address, phoneNumber, timeStart, timeEnd }) {
-    phoneNumber = '02 90342562';
-    address = 'via Malaga, 6';
-    timeStart = '12:00';
-    timeEnd =  '14:00';
+  template({ address, phoneNumber }) {
     return (
       <FlexView className='restaurant-details-header' hAlignContent='center' vAlignContent='center'>
         <FlexView className='detail'>
@@ -37,10 +30,10 @@ export default class RestaurantDetailsHeader extends React.Component {
           <i className='location-icon'></i>
           <p className='address'>INDIRIZZO: {address}</p>
         </FlexView>
-        <FlexView className='detail'>
+        {/*<FlexView className='detail'>
           <i className='time-icon'></i>
           <p className='address'>FASCIA ORARIA: {timeStart} - {timeEnd}</p>
-        </FlexView>
+        </FlexView>*/}
       </FlexView>
     );
   }
--- web/src/app/components/customer/RestaurantDetailsHeader/index.js
+++ web/src/app/components/customer/RestaurantDetailsHeader/index.js
@@ -1,1 +1,1 @@
-export default from './RestaurantDetailsHeader';
+export default from './RestaurantDetailsHeaderContainer';
