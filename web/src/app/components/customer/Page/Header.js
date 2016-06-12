0d0c681c24767c49fa33c432bcd37e7352d5d71e
Francesco <firefelix@gmail.com>
2016-06-12 15:17:59 +0200

    code clean

--- web/src/app/components/customer/Page/Header.js
+++ web/src/app/components/customer/Page/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { props, t } from 'tcomb-react';
+import { props } from 'tcomb-react';
 import { pure, skinnable } from 'revenge';
 import FlexView from 'FlexView';
 
@@ -16,18 +16,30 @@ import './img/logo.png';
   */
 })
 export default class Header extends React.Component {
-  template({ title, subtitle, imgURL }) {
-    title = 'ACCOMODATI IL TUO PIATTO TI ASPETTA';
-    subtitle = 'We are cooking up something great for you.';
-    imgURL = 'https://volo-images.s3.amazonaws.com/production/it/s3qv-hero.jpg?3';
+  template({
+    title = 'ACCOMODATI IL TUO PIATTO TI ASPETTA',
+    subtitle = 'We are cooking up something great for you.',
+    imgURL = 'https://volo-images.s3.amazonaws.com/production/it/s3qv-hero.jpg?3'
+  }) {
     return (
       <FlexView className='header'style={{ backgroundImage: `url(${imgURL})` }}>
         <FlexView className='header-overlay' column hAlignContent='center' vAlignContent='top' grow>
-          <FlexView className='header-container header-container-logo' column hAlignContent='left' vAlignContent='top'>
-            <span className="logo"></span>
+          <FlexView
+            className='header-container header-container-logo'
+            column
+            hAlignContent='left'
+            vAlignContent='top'
+          >
+            <span className='logo' />
           </FlexView>
-          <FlexView className='header-container' column hAlignContent='center' vAlignContent='center' grow>
-            <h1 className="header-title">{title}</h1>
+          <FlexView
+            className='header-container'
+            column
+            hAlignContent='center'
+            vAlignContent='center'
+            grow
+          >
+            <h1 className='header-title'>{title}</h1>
             <p className='header-subtitle'>{subtitle}</p>
           </FlexView>
         </FlexView>
