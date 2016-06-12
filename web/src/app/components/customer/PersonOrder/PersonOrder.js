6edd933a3b01b4c40d2d9ee3f3fabe3ea94aa920
Francesco <firefelix@gmail.com>
2016-06-12 16:49:14 +0200

    implement PersonOrder

--- web/src/app/components/customer/PersonOrder/PersonOrder.js
+++ web/src/app/components/customer/PersonOrder/PersonOrder.js
@@ -1,5 +1,40 @@
 import React from 'react';
+import t from 'tcomb';
+import { props } from 'tcomb-react';
+import { skinnable } from 'revenge';
+import { FlexView } from 'Basic';
+import OrderDetails from 'customer/OrderDetails';
 
+@skinnable()
+@props({
+  personItems: t.list(t.String),
+  personId: t.String,
+  onCancel: t.Function,
+  onConfirm: t.Function
+})
 export default class PersonOrder extends React.Component {
-  render = () => <div>PersonOrder</div>
+
+  getLocals({ personItems, personId, onCancel, onConfirm }) {
+    return {
+      onCancel,
+      onConfirm,
+      orderDetailsProps: {
+        people: [{ name: personId, items: personItems }]
+      }
+    };
+  }
+
+  template({ orderDetailsProps, onCancel, onConfirm }) {
+    return (
+      <FlexView className='order' grow column>
+        <OrderDetails {...orderDetailsProps} />
+        <button onClick={onCancel}>
+          Annulla
+        </button>
+        <button className='primary' onClick={onConfirm}>
+          Aggiungi ordine
+        </button>
+      </FlexView>
+    );
+  }
 }
--- web/src/app/components/customer/PersonOrder/PersonOrderContainer.js
+++ web/src/app/components/customer/PersonOrder/PersonOrderContainer.js
@@ -1,6 +1,14 @@
 import container from 'container';
+import t from 'tcomb';
 import PersonOrder from './PersonOrder';
 
 export default container(PersonOrder, {
-
+  connect: { personItems: t.maybe(t.list(t.String)), personId: t.maybe(t.String) },
+  commands: ['doAddPersonToOrder'],
+  mapProps: ({ personItems = [], personId, transition, doAddPersonToOrder }) => ({
+    personItems,
+    personId,
+    onCancel: () => transition({ view: 'order', personId: null, personItems: null }),
+    onConfirm: doAddPersonToOrder
+  })
 });
