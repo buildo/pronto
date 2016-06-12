546a7863b8bbbfad7d4ff8e6014706db10cb42ab
Francesco <firefelix@gmail.com>
2016-06-12 19:45:19 +0200

    fix: move RestaurantDetailsHeader to RestaurantHandler

--- web/src/app/routes/customer/RestaurantHandler.js
+++ web/src/app/routes/customer/RestaurantHandler.js
@@ -4,12 +4,14 @@ import Page, { Content, Footer } from 'customer/Page';
 import RightSidebar from 'customer/RightSidebar';
 import Menu from 'customer/Menu';
 import RestaurantHeader from 'customer/RestaurantHeader';
+import RestaurantDetailsHeader from 'customer/RestaurantDetailsHeader';
 
 export default class RestaurantHandler extends React.Component {
   render() {
     return (
       <Page>
         <RestaurantHeader />
+        <RestaurantDetailsHeader />
         <Content>
           <Menu />
           <RightSidebar>
--- web/src/app/routes/customer/RestaurantsHandler.js
+++ web/src/app/routes/customer/RestaurantsHandler.js
@@ -2,15 +2,12 @@ import React from 'react';
 import Page, { Content, Footer } from 'customer/Page';
 import Restaurants from 'customer/Restaurants';
 import RestaurantsHeader from 'customer/RestaurantsHeader';
-import RestaurantDetailsHeader from 'customer/RestaurantDetailsHeader';
-
 
 export default class RestaurantsHandler extends React.Component {
   render() {
     return (
       <Page>
         <RestaurantsHeader />
-        <RestaurantDetailsHeader />
         <Content>
           <Restaurants />
         </Content>
