bc0c469fb00e57de06f3816dfd7741de03250149
Francesco <firefelix@gmail.com>
2016-06-12 21:44:12 +0200

    commit deploy script

---
+++ web/deploy.sh
@@ -0,0 +1,14 @@
+# brew install awscli
+# aws configure (insert keys, set region to eu-central-1)
+
+# modify API_ENDPOINT in config.js to 'http://oxway-prod.elasticbeanstalk.com'
+
+npm run build
+
+# deploy "customer" app
+aws s3 sync --acl public-read --content-encoding gzip --exclude '*' --include '*.gz' build/customer s3://pronto-customer
+aws s3 sync --acl public-read --exclude '*.gz' build/customer s3://pronto-customer
+
+# deploy "restaurant" app
+aws s3 sync --acl public-read --content-encoding gzip --exclude '*' --include '*.gz' build/restaurant s3://pronto-restaurant
+aws s3 sync --acl public-read --exclude '*.gz' build/restaurant s3://pronto-restaurant
