4a3d009d5f4bba9923db863065659bfad2d499a1
Marco Fumagalli <fuma900@gmail.com>
2016-06-12 12:29:20 +0200

    created header

--- web/src/app/components/customer/Page/Header.js
+++ web/src/app/components/customer/Page/Header.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import { FlexView } from 'Basic';
+import { props, t } from 'tcomb-react';
+import { pure, skinnable } from 'revenge';
+import FlexView from 'FlexView';
 
 import './header.scss';
 
+const imgURL = 'https://placehold.it/1200x1200';
+
+@pure
+@skinnable()
+@props({
+  /*
+  title: t.String,
+  subtitle: t.String,
+  imgURL: t.String
+  */
+})
 export default class Header extends React.Component {
-  render = () => (
-    <FlexView className='header'>
-      {this.props.children}
-    </FlexView>
-  )
+  template({ title, subtitle, imgURL }) {
+    title = 'Almost Pronto!';
+    subtitle = 'We are cooking up something great for you.';
+    imgURL = 'https://placehold.it/1200x1200';
+    return (
+      <FlexView className='header'>
+        <img src={imgURL} alt='' className="headerImage" />
+        <h1 className="header-title">{title}</h1>
+        <p className='header-subtitle'>{subtitle}</p>
+      </FlexView>
+    );
+  }
 }
