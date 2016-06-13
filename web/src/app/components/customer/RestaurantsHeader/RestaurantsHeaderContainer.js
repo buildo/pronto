e34c9f515b83cd9c5d6e0fd19a9abf6fd7edf459
Francesco <firefelix@gmail.com>
2016-06-13 21:26:35 +0200

    style fixes

--- web/src/app/components/customer/RestaurantsHeader/RestaurantsHeaderContainer.js
+++ web/src/app/components/customer/RestaurantsHeader/RestaurantsHeaderContainer.js
@@ -5,7 +5,7 @@ const RestaurantsHeaderContainer = container(Header, {
   mapProps: () => ({
     title: 'ACCOMODATI IL TUO PIATTO TI ASPETTA',
     subtitle: 'We are cooking up something great for you.',
-    imgUrl: 'https://volo-images.s3.amazonaws.com/production/it/s3qv-hero.jpg?3'
+    imgUrl: 'https://s32.postimg.org/rl5ylkzph/group_2x.png'
   })
 });
 
